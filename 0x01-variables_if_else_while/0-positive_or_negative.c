a1bdb2b46cbdd7dec444c3e4e8fceac4e8237a95
root <adzoangela>
2023-03-06 06:18:07 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-rintf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
@@ -23,7 +23,7 @@ printf("%d is zero\n", n);
 }
 else
 {
-rintf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
