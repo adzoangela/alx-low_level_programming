873e57cdea15c3110d353bcbe3daecd67a5da98a
root <adzoangela>
2023-03-06 19:08:54 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,17 +14,18 @@ for (i = 0; i < 9; i++)
 for (j = i + 1; j <= 9; j++)
 {
 putchar(i + '0');
-putchar(',');
-putchar(' ');
 putchar(j + '0');
 if (i != 8 || j != 9)
 {
 putchar(',');
 putchar(' ');
 }
-}
-}
+else
+{
 putchar('\n');
+}
+}
+}
 
 return (0);
 }
