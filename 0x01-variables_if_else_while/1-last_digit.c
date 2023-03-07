8e96cf81ceac43a5802e0cb3cd312e91ec9abc9a
root <adzoangela>
2023-03-06 16:31:00 -0800

    edit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,16 +3,31 @@
 #include <stdio.h>
 
 /**
-** main- Prints the alphabetic
+** main- Prints a text according number
 ** Return: Always (Success)
 */
 
 int main(void)
 {
+int n, lastd;
 
-char c;
-for (c = 'a'; c <= 'z'; c++)
-putchar(c);
-putchar('\n');
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastd = n % 10;
+
+printf("Last digit of %d is ", n);
+
+if (lastd > 5)
+{
+printf("%d and is greater than 5\n", lastd);
+}
+else if (lastd == 0)
+{
+printf("%d and is 0\n", lastd);
+}
+else
+{
+printf("%d and is less than 6 and not 0\n", lastd);
+}
 return (0);
 }
