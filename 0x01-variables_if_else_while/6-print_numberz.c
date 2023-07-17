c6d500fd8b1b8f6532cf713620c5f9889206a317
adzoangela <angelamawufemor@gmail.com>
2023-07-16 17:20:22 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the numeric digits 0 to 9 as characters
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
 
 	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
+		putchar(n + '0');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
