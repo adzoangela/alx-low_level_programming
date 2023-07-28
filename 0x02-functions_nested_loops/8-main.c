b300d1a1a08344deacc54d9759ba10f8fd0c4eaf
adzoangela <angelamawufemor@gmail.com>
2023-07-27 21:39:33 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function calls the jack_bauer() function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
