db7d8149bfdf2b9ee1f3e14eb5bae851639a9bd0
adzoangela <angelamawufemor@gmail.com>
2023-08-12 20:22:34 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns  the length of a string
+ * @s: string to be returned
+ * Return: 0 or lenght
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
