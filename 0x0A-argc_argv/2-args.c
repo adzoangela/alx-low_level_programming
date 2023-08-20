6d43de8b63382a15d9edb748b11e21e892be5716
adzoangela <angelamawufemor@gmail.com>
2023-08-20 16:12:09 -0700

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
